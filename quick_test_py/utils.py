270a2d4ba3436eb8df7699554588882915aad5cc
elv-nickB <nick.buoncristiani@eluv.io>
2024-08-21 12:10:50 +0000

    Added more helpful error message when == does not return bool

--- quick_test_py/utils.py
+++ quick_test_py/utils.py
@@ -1,7 +1,12 @@
 import numpy as np
 
 def are_equal(value1, value2):
-    if isinstance(value1, np.ndarray) or isinstance(value2, np.ndarray):
-        return np.array_equal(value1, value2)
-    else:
-        return value1 == value2
+    try:
+        if isinstance(value1, np.ndarray) or isinstance(value2, np.ndarray):
+            return np.array_equal(value1, value2)
+        else:
+            return value1 == value2
+    except Exception as e:
+        raise ValueError(f"The output and ground truth are not able to be compared directly via the == operator. \
+                         It may be worth converting the output of the test case function to a type such that \
+                         the == operator returns a bool.") from e
